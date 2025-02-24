397047fa6b42f6d896880fbefa383c91915c9e7c
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2025-02-24 17:49:16 +0100

    modified some script for the task 2 and the task 3 (users and amenity)

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,18 +1,57 @@
 from app.persistence.repository import InMemoryRepository
+from app.models.user import User
+from app.models.amenity import Amenity
 
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
-        self.place_repo = InMemoryRepository()
-        self.review_repo = InMemoryRepository()
-        self.amenity_repo = InMemoryRepository()
+        self.amenity_repo = InMemoryRepository()  # Add amenity repository
 
-    # Placeholder method for creating a user
     def create_user(self, user_data):
-        # Logic will be implemented in later tasks
-        pass
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
 
-    # Placeholder method for fetching a place by ID
-    def get_place(self, place_id):
-        # Logic will be implemented in later tasks
-        pass
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email):
+        return self.user_repo.get_by_attribute('email', email)
+
+    def get_all_users(self):
+        """Retrieve all users from the repository"""
+        return self.user_repo.get_all()
+
+    def update_user(self, user_id, user_data):
+        """Update user with new data"""
+        user = self.get_user(user_id)
+        if user:
+            for key, value in user_data.items():
+                setattr(user, key, value)
+            self.user_repo.update(user_id, user_data)
+            return user
+        return None
+
+    def create_amenity(self, amenity_data):
+        """Create a new amenity"""
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        """Get an amenity by ID"""
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        """Get all amenities"""
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        """Update an amenity"""
+        amenity = self.get_amenity(amenity_id)
+        if amenity:
+            for key, value in amenity_data.items():
+                setattr(amenity, key, value)
+            self.amenity_repo.update(amenity_id, amenity_data)
+            return amenity
+        return None
